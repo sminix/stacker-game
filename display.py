7719de9b2b6fbbdf99fdcb34ca3611cedbb62d09
sminix <48739178+sminix@users.noreply.github.com>
2020-06-13 18:44:49 -0500

    Update display.py
    clears terminal each display call

--- display.py
+++ display.py
@@ -4,19 +4,22 @@ Sam Minix
 
 Controls the terminal display using curses
 '''
-
+import sys
 import time
 
 def display(gameState):
-  for i in range(15):
+  time.sleep(.5)
+  sys.stdout.write("\x1b[f\x1b[J") #clears output and sets cursor in top corner from https://stackoverflow.com/questions/51315269/multithreading-curses-output-in-python
+  for i in range(15): 
     for j in range(9):
-      print(gameState[i][j], end='')
+      sys.stdout.write(str(gameState[i][j]))
     if i == 0:
-      print(" Major Prize!", end='')
+      sys.stdout.write(" Major Prize!")
     if i == 5:
-      print(" Minor Prize", end='')
+      sys.stdout.write(" Minor Prize")
     if i == 14:
-      print("Start", end='')
-    print()
+      sys.stdout.write("Start")
+    sys.stdout.write('\n')
+    sys.stdout.flush() #flushes output before next row
 
 
